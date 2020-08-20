262c7c7f5792210d70aa5f37a8b078a00ce6774e
Darkuus <vitork42@gmail.com>
2020-08-20 03:05:43 -0300

    add enviroment variable fo heroku

--- src/Services/api.ts
+++ src/Services/api.ts
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    //baseURL: 'https://amc-api.herokuapp.com/',
-    baseURL: 'http://localhost:3333',
+    baseURL: process.env.REACT_APP_API_URL,
     responseType: 'json',
     headers: {
         'Content-Type': 'application/json'
